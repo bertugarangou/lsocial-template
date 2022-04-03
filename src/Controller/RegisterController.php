9320eb33985cae2e1afc3a5486073b101c275369
Albert Garangou Culebras <albertgarangou@gmail.com>
2022-04-03 14:14:07 +0200

    nem fent

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,8 +1,10 @@
 <?php
+# Gràcies stackoverflow 😶
 declare(strict_types=1);
 
 namespace Salle\LSocial\Controller;
 
+use Salle\LSocial\Model\User;
 use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -33,20 +35,46 @@ final class RegisterController{
         $errors = [];
 
         #mirar email
-        if (empty($data['email']) || !filter_var($data['email'], FILTER_VALIDATE_EMAIL) || strpos($data['email'],"@salle.url.edu") == false || $this->SQLRepo->checkEmailExists($data['email']) == true) {
-            $errors['email'] = 'The email address is not valid';
-        }
+
+        if(empty($data['email']) || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+            $errors['email'] = 'The email address is not valid.';
+        }else if($this->emailLassallista($data['email']) == false){
+            $errors['email'] = 'Only emails from the domain @salle.url.edu are accepted.';
+        }else if($this->SQLRepo->checkEmailExists($data['email']) == true){
+            $errors['email'] = 'User already exists. Email must be unique.';
+        }#TODO: bbdd mirar si està repetit
+
 
         #mirar contra
-        if(empty($data['passwd']) || strlen($data['passwd']) < 5 || preg_match('/[A-Z]/',$data['passwd']) != 1 || preg_match('/[a-z]/',$data['passwd']) != 1){
-            $errors['passwd'] = 'The password is not valid';
+        if(empty($data['passwd']) || strlen($data['passwd']) < 5){
+            $errors['passwd'] = 'The password must contain at least 6 characters.';
+        }else if(preg_match('/[A-Z]/',$data['passwd']) != 1 || preg_match('/[a-z]/',$data['passwd']) != 1 || preg_match('/[0-9]/',$data['passwd']) != 1){
+            $errors['passwd'] = 'The password must contain both upper and lower case letters and numbers.';
+        }else if(strcmp($data['passwd'], $data['passwd2']) != 0){
+            $errors['passwd'] = 'Passwords are not equal';
         }
+
+        #mirar birthdate
+        if(!empty($data['birth'])){
+            if($this->validateDate($data['birth']) == false){
+                $errors['birth'] = 'Birthday is invalid.';
+            }else if($this->validateAge($data['birth']) == false){
+                $errors['birth'] = 'Sorry, you are underage.';
+                var_dump($data['birth']);
+            }
+        }
+
 
 
         if(count($errors) == 0){#està nais 👌
-            return $this->twig->render($response,'home.twig',[
+
+            $user = new User($data['email'], $data['passwd'], $data['birth']);
+            $this->SQLRepo->save($user);
+
+            return $this->twig->render($response,'login.twig',[
                 'formMethod' => "GET"
-            ]);
+            ]); #TODO: canviar link
+
         }else {#errors sad uwu
 
 
@@ -62,5 +90,27 @@ final class RegisterController{
                 ]
             );
         }
+    }
+    private function validateDate($birthday): bool{
+        if(is_string($birthday)) $birthday = strtotime($birthday);
+        if($birthday - time() >= -86400) return false; # si es tria el dia actual dona error, that's ok
+        return true;
+    }
+
+    private function validateAge($birthday):bool{
+        if(is_string($birthday)) $birthday = strtotime($birthday);
+        if(time() - $birthday < 18 * 31556926) return false;
+        return true;
+    }
+
+    private function emailLassallista(mixed $email): bool{
+
+        #ja hi haurà quelcom dins la string si arriba
+        $troncar = explode('@',$email);
+        $domini = array_pop($troncar);
+        if(strcmp($domini, "salle.url.edu") == 0){
+            return true;
+        }
+        return false;
     }
 }
--- src/Model/User.php
+++ src/Model/User.php
@@ -10,63 +10,14 @@ final class User
     private int $id;
     private string $email;
     private string $password;
-    private DateTime $birthday;
-    private DateTime $createdAt;
-    private DateTime $updatedAt;
+    private String $birthday;
+    #created i modified no es fan servir
 
-    public function __construct(
-        string   $email,
-        string   $password,
-        DateTime $birthday,
-        DateTime $createdAt,
-        DateTime $updatedAt
-    )
+
+    public function __construct(string $email,string $password,string $birthday,)
     {
         $this->email = $email;
         $this->password = $password;
-        $this->createdAt = $createdAt;
-        $this->updatedAt = $updatedAt;
-
-        if(strlen($birthday->format('Y-m-d H:i:s')) == 0){#si no hi ha birthday
-            $birthday = NULL;
-        }
-        $this->birthday = $birthday;
-
-
-    }
-
-    public function id(): int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-    public function email(): string
-    {
-        return $this->email;
-    }
-
-    public function password(): string
-    {
-        return $this->password;
-    }
-
-    public function createdAt(): DateTime
-    {
-        return $this->createdAt;
-    }
-
-    public function updatedAt(): DateTime
-    {
-        return $this->updatedAt;
-    }
-    public function birthday(): DateTime
-    {
-        return $this->birthday;
+        $this->birthday = $birthday; #si es null ja es mirarà a la bbdd
     }
 }
